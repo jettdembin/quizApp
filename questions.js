9e81f39b4beb2213c94785f937ef7151ceb23987
Jett Dembin <71148367+jettdembin@users.noreply.github.com>
2021-08-27 16:44:48 -0400

    Separete file for quesitons

---
+++ questions.js
@@ -0,0 +1,22 @@
+var questions = [{
+    "question" : "Which of the following is not a real eCommerce platform?",
+    "option1" : "Shopify",
+    "option2" : "WooCommerce",
+    "option3" : "ShopCommerce",
+    "option4" : "BigCommerce",
+    "answer" : "3"
+}, {
+    "question" : "If Shopify is so good, why are Shopify developers necessary?",
+    "option1" : "To save time on things like store setups and migrations",
+    "option2" : "To extend the limited design options and functionalities of themes with custom code",
+    "option3" : "To provide support with a deep understanding of how the platform works and what its limitations are",
+    "option4" : "All the above",
+    "answer" : "4"
+}, {
+    "question" : "Which of the following is true about Shopify developers?",
+    "option1" : "They are paid extremely well",
+    "option2" : "There is a high demand for them",
+    "option3" : "They need to know web development, the platform itself, and the liquid template language",
+    "option4" : "All the above",
+    "answer" : "4"
+}];
