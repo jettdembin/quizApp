975c0659379c35c5812b95f2554099547ae454a2
Jett Dembin <71148367+jettdembin@users.noreply.github.com>
2021-08-27 16:43:53 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,49 +1,35 @@
 var currentQuestion = 0;
 var score = 0;
-var totalQuestionCount = 0;
+var totalQuestionCount = questions.length;
 
 
 
 var btn1 = document.getElementById('btn1');
 var btn2 = document.getElementById('btn2');
 var btn3 = document.getElementById('btn3');
+var back1 = document.getElementById('back-1');
+var back2 = document.getElementById('back-2');
 var question2 = document.getElementById("question-cntr2");
 var question3 = document.getElementById("question-cntr3");
+
+
 
 //function
 function totalScore() {
     var selectedOption = document.querySelector('input[type=radio]:checked');
-    for (var question of questions) {
-        var answer = selectedOption.value;
-        if (answer === question[answer]) {
-            score++
-        }
-        return score
+    if (!selectedOption) {
+        alert("Please select an answer before proceeding");
+        return
     }
+    var answer = selectedOption.value;
+    if (questions[currentQuestion].answer == answer) {
+        score++;
+    }
+    selectedOption.checked = false;
+    setNewActive();
+    currentQuestion++;
 };
 
-var questions = [{
-    "question" : "Which of the following is not a real eCommerce platform?",
-    "option1" : "Shopify",
-    "option2" : "WooCommerce",
-    "option3" : "ShopCommerce",
-    "option4" : "BigCommerce",
-    "answer" : "3"
-}, {
-    "question" : "If Shopify is so good, why are Shopify developers necessary?",
-    "option1" : "To save time on things like store setups and migrations",
-    "option2" : "To extend the limited design options and functionalities of themes with custom code",
-    "option3" : "To provide support with a deep understanding of how the platform works and what its limitations are",
-    "option4" : "All the above",
-    "answer" : "4"
-}, {
-    "question" : "Which of the following is true about Shopify developers?",
-    "option1" : "They are paid extremely well",
-    "option2" : "There is a high demand for them",
-    "option3" : "They need to know web development, the platform itself, and the liquid template language",
-    "option4" : "All the above",
-    "answer" : "4"
-}];
 
 /*
 Title: eCommerce Quiz
@@ -69,24 +55,30 @@ Question 3: Which of the following is true about Shopify developers?
 */ 
 
       // funciton to remove q-active from all hide classes and set q-active on active quesiton
-function setNewActive1() {
-    totalScore();
+function setNewActive() {
     var hiddenQuestions = document.getElementsByClassName('hide');
     for (var hiddenQuestion of hiddenQuestions) {
         hiddenQuestion.classList.remove('q-active');
     }
         // set q-active on active quesiton
-    question2.classList.add('q-active');
-    btn2.classList.add('q-active');
+    if (currentQuestion == 0) {
+        question2.classList.add('q-active');
+        btn2.classList.add('q-active');
+        back1.classList.add('q-active');
+    }
+    if (currentQuestion == 1) {
+        question3.classList.add('q-active');
+        btn3.classList.add('q-active');
+    }
 };
 
       //function onclick for button one to change to next question
 btn1.addEventListener('click', function() {
-    setNewActive1()
+    totalScore();
 });
 
+/*
 function setNewActive2() {
-    totalScore();
     var hiddenQuestions = document.getElementsByClassName('hide');
     for (var hiddenQuestion of hiddenQuestions) {
         hiddenQuestion.classList.remove('q-active');
@@ -94,17 +86,18 @@ function setNewActive2() {
         // set q-active on active quesiton
     question3.classList.add('q-active');
     btn3.classList.add('q-active');
-};
+};*/
 
       //function onclick for button one to change to next question
 btn2.addEventListener('click', function() {
-    setNewActive2()
+    totalScore();
     btn3.textContent = "Submit";
 });
 
 btn3.addEventListener('click', function() {
     totalScore();
     document.getElementById("container").style.cssText = "opacity:.4";
+    document.getElementById("heading").style.cssText = "text-align:center";
     document.getElementById('result').style.cssText = "display:block";
     document.getElementById("scored").textContent = score;
 })
